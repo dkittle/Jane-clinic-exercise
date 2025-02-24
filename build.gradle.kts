94090b67ab3db48314aba590df8f188ed2d009bb
Don Kittle <don@kittle.ca>
2025-02-24 15:26:12 -0500

    Target Java17 language version, limit junit platform launcher to test runtime only

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,12 @@ plugins {
     id("java")
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
 group = "ca.kittle"
 version = "1.0-SNAPSHOT"
 
@@ -12,6 +18,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.test {
