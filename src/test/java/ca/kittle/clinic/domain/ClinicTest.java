2e9e79179d4c377c64dac1990cfb3758ee32578a
Don Kittle <don@kittle.ca>
2025-02-26 02:18:27 -0500

    Extract Clinic Hours, simplify Clinic class, create test clinic fixture
    Clinic Hours are used to validate Booking business rules

--- src/test/java/ca/kittle/clinic/domain/ClinicTest.java
+++ src/test/java/ca/kittle/clinic/domain/ClinicTest.java
@@ -1,96 +1,63 @@
 package ca.kittle.clinic.domain;
 
+import ca.kittle.clinic.domain.fixtures.TestClinic;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.UUID;
-
+import static ca.kittle.clinic.domain.fixtures.TestClinic.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class ClinicTest {
 
-    private static final String CLINIC_NAME = "Test Clinic";
-    private static final String PHONE_NUMBER = "123-456-7890";
-    private static final String EMAIL = "testclinic@email.com";
 
     @Test
     @DisplayName("Should be able to create a clinic with valid data")
     void shouldCreateClinicWithValidData() {
-        Clinic clinic = new Clinic(CLINIC_NAME, PHONE_NUMBER, EMAIL);
+        Clinic clinic = TestClinic.TEST_CLINIC;
         assertEquals(CLINIC_NAME, clinic.getName());
         assertEquals(PHONE_NUMBER, clinic.getPhoneNumber());
         assertEquals(EMAIL, clinic.getEmail());
     }
 
-    @Test
-    @DisplayName("Should be able to create a clinic with valid data including ID")
-    void shouldCreateClinicWithValidDataIncId() {
-        UUID id = UUID.randomUUID();
-        Clinic clinic = new Clinic(id, CLINIC_NAME, PHONE_NUMBER, EMAIL);
-        assertEquals(id, clinic.getId());
-        assertEquals(CLINIC_NAME, clinic.getName());
-        assertEquals(PHONE_NUMBER, clinic.getPhoneNumber());
-        assertEquals(EMAIL, clinic.getEmail());
-    }
 
     @Test
     @DisplayName("Should throw exception when clinic name is null")
     void shouldThrowExceptionWhenNameIsNull() {
-        UUID id = UUID.randomUUID();
         assertThrows(IllegalArgumentException.class,
                 () -> new Clinic(null, PHONE_NUMBER, EMAIL)
-        );
-        assertThrows(IllegalArgumentException.class,
-                () -> new Clinic(id, null, PHONE_NUMBER, EMAIL)
         );
     }
 
     @Test
     @DisplayName("Should throw exception when clinic phone number is null")
     void shouldThrowExceptionWhenPhoneNumberIsNull() {
-        UUID id = UUID.randomUUID();
         assertThrows(IllegalArgumentException.class,
                 () -> new Clinic(CLINIC_NAME, null, EMAIL)
-        );
-        assertThrows(IllegalArgumentException.class,
-                () -> new Clinic(id, CLINIC_NAME, null, EMAIL)
         );
     }
 
     @Test
     @DisplayName("Should throw exception when clinic phone number is invalid")
     void shouldThrowExceptionWhenPhoneNumberIsInvalid() {
-        UUID id = UUID.randomUUID();
         assertThrows(IllegalArgumentException.class,
                 () -> new Clinic(CLINIC_NAME, "4165551212", EMAIL)
-        );
-        assertThrows(IllegalArgumentException.class,
-                () -> new Clinic(id, CLINIC_NAME, "4165551212", EMAIL)
         );
     }
 
     @Test
     @DisplayName("Should throw exception when clinic email is null")
     void shouldThrowExceptionWhenEmailIsNull() {
-        UUID id = UUID.randomUUID();
         assertThrows(IllegalArgumentException.class, () ->
                 new Clinic(CLINIC_NAME, PHONE_NUMBER, null)
-        );
-        assertThrows(IllegalArgumentException.class, () ->
-                new Clinic(id, CLINIC_NAME, PHONE_NUMBER, null)
         );
     }
 
     @Test
     @DisplayName("Should throw exception when clinic email is invalid")
     void shouldThrowExceptionWhenEmailIsInvalid() {
-        UUID id = UUID.randomUUID();
         assertThrows(IllegalArgumentException.class, () ->
                 new Clinic(CLINIC_NAME, PHONE_NUMBER, "clinic-email.com")
-        );
-        assertThrows(IllegalArgumentException.class, () ->
-                new Clinic(id, CLINIC_NAME, PHONE_NUMBER, "clinic-email.com")
         );
     }
 }
